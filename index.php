92b11737fe058b2b89c59dfe5b841b4686f9e46c
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2022-02-22 14:04:10 +0700

    create form input data mahasiswa

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input Data Mahasiswa</title>
+</head>
+<body>
+    <form action="/" method="post">
+        <h1>Input Data Mahasiswa Baru</h1>
+        <label for="nama">Nama</label>
+        <input type="text" name="nama">
+        <label for="nim">NIM</label>
+        <input type="text" name="nim">
+        <label for="umur">Umur</label>
+        <input type="number" name="umur">
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
